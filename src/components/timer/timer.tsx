799dfd3e139abf442854e21917b171abc6e62a9e
Bruno Henrique <brunohc2016@gmail.com>
2024-08-20 00:34:49 -0300

    :sparkles: crete timer desktop version

---
+++ src/components/timer/timer.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import styles from "./timer.module.scss";
+import we from "../../assets/images/23.06.jpg";
+
+export default function Timer() {
+  const [timeElapsed, setTimeElapsed] = useState({
+    months: 0,
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const startDate = new Date("2024-03-23T00:00:00").getTime();
+
+    const updateTimer = () => {
+      const now = new Date().getTime();
+      const diff = now - startDate;
+
+      const months = Math.floor(diff / (1000 * 60 * 60 * 24 * 30));
+      const days = Math.floor((diff / (1000 * 60 * 60 * 24)) % 30);
+      const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+      const minutes = Math.floor((diff / (1000 * 60)) % 60);
+      const seconds = Math.floor((diff / 1000) % 60);
+
+      setTimeElapsed({ months, days, hours, minutes, seconds });
+    };
+
+    updateTimer();
+    const interval = setInterval(updateTimer, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className={styles.timerContainer}>
+      <img src={we} alt="We" />
+      <div className={styles.timerText}>
+        <h1>Duda Gaymer</h1>
+        <h2>Nós estamos juntos há:</h2>
+        <div className={styles.time}>
+          <span>{timeElapsed.months} meses...</span>
+          <span>{timeElapsed.days} dias...</span>
+          <span>{timeElapsed.hours} horas...</span>
+          <span>{timeElapsed.minutes} minutos...</span>
+          <div className={styles.secondsContainer}>
+            <div key={timeElapsed.seconds} className={styles.seconds}>
+              {timeElapsed.seconds}
+            </div>
+            <span>segundos</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,13 +1,23 @@
 import styles from "./home.module.scss";
 import { polaroids } from "../../mock/polaroid";
+import Timer from "../../components/timer/timer";
 import Polaroid from "../../components/polaroid/polaroid";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <div className={styles.timerContainer}>
+        <Timer />
+      </div>
       <div className={styles.polaroidContainer}>
         {polaroids.map((item, idx) => (
-          <Polaroid key={idx} id={item.id} date={item.date} image={item.image} description={item.description}/>
+          <Polaroid
+            key={idx}
+            id={item.id}
+            date={item.date}
+            image={item.image}
+            description={item.description}
+          />
         ))}
       </div>
     </main>
