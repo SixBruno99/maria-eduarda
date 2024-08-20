ffd8d9136afeccccbf9e175f73e6eb61720c50f8
Bruno Henrique <brunohc2016@gmail.com>
2024-08-19 23:02:17 -0300

    :sparkles: create desktop version

--- src/mock/polaroid.ts
+++ src/mock/polaroid.ts
@@ -7,10 +7,46 @@ import july from "../assets/images/07.jpeg";
 import august from "../assets/images/08.jpeg";
 
 export const polaroids: TPolaroid[] = [
-  { id: "1", date: "Março", image: march, description: "" },
-  { id: "2", date: "Abril", image: april, description: "" },
-  { id: "3", date: "Maio", image: may, description: "" },
-  { id: "4", date: "Junho", image: june, description: "" },
-  { id: "5", date: "Julho", image: july, description: "" },
-  { id: "6", date: "Agosto", image: august, description: "" },
+  {
+    id: "1",
+    date: "Março",
+    image: march,
+    description:
+      "Março, o mês que te conheci, o mês que você pegou meu coração e decidiu cuidar dele como se fosse seu.",
+  },
+  {
+    id: "2",
+    date: "Abril",
+    image: april,
+    description:
+      "Abril, o mês que percebi que meu sonho havia se realizado, ter ao meu lado uma garota incrível e que me faz tão bem.",
+  },
+  {
+    id: "3",
+    date: "Maio",
+    image: may,
+    description:
+      "Maio, o mês que eu tive certeza de que quero estar ao seu lado pro resto da vida.",
+  },
+  {
+    id: "4",
+    date: "Junho",
+    image: june,
+    description:
+      "Junho, o mês que concretizamos tudo e assumimos um compromisso um com o outro.",
+  },
+  {
+    id: "5",
+    date: "Julho",
+    image: july,
+    description:
+      "Julho, o mês que descobri que não importa o que aconteça, você sempre estará comigo me apoiando.",
+  },
+  {
+    id: "6",
+    date: "Agosto",
+    image: august,
+    description:
+      "Agosto, o mês que vi que todos os momentos são perfeitos quando estou ao seu lado.",
+  },
 ];
