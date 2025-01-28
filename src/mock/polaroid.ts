f30348c1b970953171177248e85b2632938b2c3b
Bruno Henrique <brunohc2016@gmail.com>
2025-01-28 12:00:07 -0300

    :art: add a new month

--- src/mock/polaroid.ts
+++ src/mock/polaroid.ts
@@ -1,82 +1,92 @@
 import { TPolaroid } from "../types/polaroid";
 
-const path = "/src/assets/images";
+import march2024 from "../assets/images/2024/03.jpeg";
+import april2024 from "../assets/images/2024/04.jpeg";
+import may2024 from "../assets/images/2024/05.jpeg";
+import june2024 from "../assets/images/2024/06.jpeg";
+import july2024 from "../assets/images/2024/07.jpeg";
+import august2024 from "../assets/images/2024/08.jpeg";
+import september2024 from "../assets/images/2024/09.jpeg";
+import october2024 from "../assets/images/2024/10.jpeg";
+import november2024 from "../assets/images/2024/11.jpeg";
+import december2024 from "../assets/images/2024/12.jpeg";
+import january2025 from "../assets/images/2025/01.jpeg";
 
 export const polaroids: TPolaroid[] = [
   {
     id: "1",
-    date: "Março 2024",
-    image: `${path}/2024/03.jpeg`,
+    date: "Março",
+    image: march2024,
     description:
       "Março, o mês que te conheci, o mês que você pegou meu coração e decidiu cuidar dele como se fosse seu.",
   },
   {
     id: "2",
-    date: "Abril 2024",
-    image: `${path}/2024/04.jpeg`,
+    date: "Abril",
+    image: april2024,
     description:
       "Abril, o mês que percebi que meu sonho havia se realizado, ter ao meu lado uma garota incrível e que me faz tão bem.",
   },
   {
     id: "3",
-    date: "Maio 2024",
-    image: `${path}/2024/05.jpeg`,
+    date: "Maio",
+    image: may2024,
     description:
       "Maio, o mês que eu tive certeza de que quero estar ao seu lado pro resto da vida.",
   },
   {
     id: "4",
-    date: "Junho 2024",
-    image: `${path}/2024/06.jpeg`,
+    date: "Junho",
+    image: june2024,
     description:
       "Junho, o mês que concretizamos tudo e assumimos um compromisso um com o outro.",
   },
   {
     id: "5",
-    date: "Julho 2024",
-    image: `${path}/2024/07.jpeg`,
+    date: "Julho",
+    image: july2024,
     description:
       "Julho, o mês que descobri que não importa o que aconteça, você sempre estará comigo me apoiando.",
   },
   {
     id: "6",
-    date: "Agosto 2024",
-    image: `${path}/2024/08.jpeg`,
+    date: "Agosto",
+    image: august2024,
     description:
       "Agosto, o mês que vi que todos os momentos são perfeitos quando estou ao seu lado.",
   },
   {
     id: "7",
-    date: "Setembro 2024",
-    image: `${path}/2024/09.jpeg`,
+    date: "Setembro",
+    image: september2024,
     description:
       "Setembro, o mês que fortalecemos nossa relação e somos capazes de resolver qualquer coisa juntos.",
   },
   {
     id: "8",
     date: "Outubro 2024",
-    image: `${path}/2024/10.jpeg`,
+    image: october2024,
     description:
       "Outubro, o mês que percebi que cada sorriso seu ilumina ainda mais os meus dias, e que nosso amor cresce a cada momento que passamos juntos.",
   },
   {
     id: "9",
     date: "Novembro 2024",
-    image: `${path}/2024/11.jpeg`,
+    image: november2024,
     description:
       "Novembro, o mês em que percebi o quanto você é minha maior inspiração, com sua força e determinação que me motivam a ser melhor a cada dia.",
   },
   {
     id: "10",
     date: "Dezembro 2024",
-    image: `${path}/2024/12.jpeg`,
+    image: december2024,
     description:
       "Dezembro, o mês em que encerramos o ano com momentos inesquecíveis, sonhando juntos com tudo que ainda viveremos.",
   },
   {
     id: "11",
     date: "Janeiro 2025",
-    image: `${path}/2025/01.jpeg`,
+    image: january2025,
     description:
       "Janeiro, o mês que revitalizamos nossa relação, cada vez mostramos mais o quanto queremos um ao outro.",
   },
