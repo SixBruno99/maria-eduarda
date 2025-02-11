fac04465a4197ef95f437bf7bc4e3f717ab5d13d
Bruno Henrique <brunohc2016@gmail.com>
2025-02-10 21:20:48 -0300

    :art: add a new month

--- src/mock/polaroid.ts
+++ src/mock/polaroid.ts
@@ -15,49 +15,49 @@ import january2025 from "../assets/images/2025/01.jpeg";
 export const polaroids: TPolaroid[] = [
   {
     id: "1",
-    date: "Março",
+    date: "Março 2024",
     image: march2024,
     description:
       "Março, o mês que te conheci, o mês que você pegou meu coração e decidiu cuidar dele como se fosse seu.",
   },
   {
     id: "2",
-    date: "Abril",
+    date: "Abril 2024",
     image: april2024,
     description:
       "Abril, o mês que percebi que meu sonho havia se realizado, ter ao meu lado uma garota incrível e que me faz tão bem.",
   },
   {
     id: "3",
-    date: "Maio",
+    date: "Maio 2024",
     image: may2024,
     description:
       "Maio, o mês que eu tive certeza de que quero estar ao seu lado pro resto da vida.",
   },
   {
     id: "4",
-    date: "Junho",
+    date: "Junho 2024",
     image: june2024,
     description:
       "Junho, o mês que concretizamos tudo e assumimos um compromisso um com o outro.",
   },
   {
     id: "5",
-    date: "Julho",
+    date: "Julho 2024",
     image: july2024,
     description:
       "Julho, o mês que descobri que não importa o que aconteça, você sempre estará comigo me apoiando.",
   },
   {
     id: "6",
-    date: "Agosto",
+    date: "Agosto 2024",
     image: august2024,
     description:
       "Agosto, o mês que vi que todos os momentos são perfeitos quando estou ao seu lado.",
   },
   {
     id: "7",
-    date: "Setembro",
+    date: "Setembro 2024",
     image: september2024,
     description:
       "Setembro, o mês que fortalecemos nossa relação e somos capazes de resolver qualquer coisa juntos.",
